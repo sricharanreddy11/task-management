10deae58eb3ab45d0a713c5cc8dac14dd8c66662
sricharanreddy11 <sricharanteegala@gmail.com>
2024-08-26 12:29:11 +0530

    updated environments

--- devlog_frontend/src/environments/environment.development.ts
+++ devlog_frontend/src/environments/environment.development.ts
@@ -1,4 +1,9 @@
+// export const environment = {
+//     production: false,
+//     apiUrl: 'http://127.0.0.1:8000/'
+//   };
+
 export const environment = {
-    production: false,
-    apiUrl: 'http://127.0.0.1:8000/'
-  };
+  production: true,
+  apiUrl: 'https://devtracker.up.railway.app/'
+};
