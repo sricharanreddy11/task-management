739643001af7c8f7bc235ba93c34856351b434f9
sricharanreddy11 <sricharanteegala@gmail.com>
2025-03-26 23:11:54 +0530

    deployment issues fixed

--- devlog_frontend/src/environments/environment.ts
+++ devlog_frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://devtracker.up.railway.app/'
-  };
+// export const environment = {
+//     production: true,
+//     apiUrl: 'https://devtracker.up.railway.app/'
+//   };
